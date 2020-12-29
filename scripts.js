5973f2b83761623a12b86753f46a5a57cc937c89
acedmiston <63328695+acedmiston@users.noreply.github.com>
2020-12-29 22:48:01 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,77 +1,124 @@
-<!DOCTYPE html>
-<html lang="en-US">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-  <link href="styles.css" rel="stylesheet" type="text/css">
-  <title>My Address Book</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid text-center" style="margin-bottom:2">
-    <h1>&#x2728; My Address Book &#x2728;</h1>
-    <h4>Feel free to add new contacts below!</h4> 
-  </div>
-  <div class="container">
-    <div class="row">
-      <div class="col-md-6">
-        <h2>&#x2728; Add a Contact &#x2728;</h2>
-        <hr>
-        <form id="new-contact">
-          <div class="form-group">
-            <label for="new-first-name">First Name</label>
-            <input type="text"  class="form-control" id="new-first-name"  placeholder="e.g. John" required>
-          </div>
-          <div class="form-group">
-            <label for="new-last-name">Last Name</label>
-            <input type="text"  class="form-control" id="new-last-name"  placeholder="e.g. Doe" required>
-          </div>
-          <div class="form-group">
-            <label for="new-phone-number">Phone Number</label>
-            <input type="text"  class="form-control" id="new-phone-number"  placeholder="e.g. (123) 456-7890" required>
-          </div>
-          <div class="form-group">
-            <label for="new-address">Address</label>
-            <input type="text"  class="form-control" id="new-address"  placeholder="e.g. 123 Main St. Somecity, Somestate 12345" required>
-          </div>
-          <button type="submit" class="btn-primary">Add</button>
-        </form>
-        <hr>
-        <h2>&#x2728; All Contacts &#x2728;</h2>
-        <hr>
-        <form id="search-person">
-            <h6>Search for a contact:</h6>
-            <input type="text" placeholder="e.g. John Doe">
-            <button id="search-button" type="button" class="btn-primary">Search</button>
-            <!-- <div class="search-bar">
-              <input type="text" name="search" value="" autocomplete="off" id="myinput" onkeyup="searchFunction()" placeholder="Search">
-            </div> -->
-          <!-- <div id="searchWrapper">
-            <input
-                type="text"
-                name="searchBar"
-                id="searchBar"
-                placeholder="search for something"
-            />
-        </div> -->
-        </form> 
-        <hr>  
-        <ul id="contacts">
-        </ul>
-      </div>
-      <div class="col-md-6">
-        <div id="show-contact">
-          <p>First Name: <span class="first-name"></span></p>
-          <p>Last Name: <span class="last-name"></span></p>
-          <p>Phone Number: <span class="phone-number"></span></p>
-          <p>Address: <span class="address"></span></p>
-          <div id="buttons">
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
-  <script src="scripts.js"></script>
-</body>
-</html>
+function AddressBook() {
+    this.contacts = {};
+    this.currentId = 0;
+}
+
+AddressBook.prototype.addContact = function(contact) {
+    contact.id = this.assignId();
+    this.contacts[contact.id] = contact;
+}
+
+//assign a unique id
+AddressBook.prototype.assignId = function() {
+    this.currentId += 1;
+    return this.currentId;
+}
+
+// find a contact
+AddressBook.prototype.findContact = function(id) {
+    if (this.contacts[id] != undefined) {
+      return this.contacts[id];
+    }
+    return "This contact does not exist";
+  }
+
+  // AddressBook.prototype.updateContact = function(id) {
+  //   if (this.contacts[id] != undefined) {
+  //     return this.contacts[id];
+  //   }
+  //   return "This contact does not exist";
+  // }
+  
+// delete a contact
+  AddressBook.prototype.deleteContact = function(id) {
+    if (this.contacts[id] === undefined) {
+      return "This contact does not exist";
+    }
+    delete this.contacts[id];
+    return true;
+  }
+
+function Contact(firstName, lastName, phoneNumber, address){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.phoneNumber = phoneNumber;
+    this.address = address;
+}
+
+// Contact.prototype.fullName = function() {
+//     return this.firstName + " " + this.lastName;
+// }
+
+let addressBook = new AddressBook();
+
+function displayContactDetails(addressBookToDisplay) {
+  let contactsList = $("ul#contacts");
+  let htmlForContactInfo = "";
+  Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+    const contact = addressBookToDisplay.findContact(key);
+    htmlForContactInfo += "<li class='people' id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>";
+  });
+  contactsList.html(htmlForContactInfo);
+};
+
+function showContact(contactId) {
+  const contact = addressBook.findContact(contactId);
+  $("#show-contact").show();
+  $(".first-name").html(contact.firstName);
+  $(".last-name").html(contact.lastName);
+  $(".phone-number").html(contact.phoneNumber);
+  $(".address").html(contact.address);
+  let buttons = $("#buttons");
+  buttons.empty();
+  buttons.append("<button class='deleteButton' id=" +  + contact.id + ">Delete</button>");
+}
+
+function attachContactListeners() {
+  $("ul#contacts").on("click", "li", function() {
+    showContact(this.id);
+  });
+
+  $("#buttons").on("click", ".deleteButton", function() {
+    addressBook.deleteContact(this.id);
+    $("#show-contact").hide();
+    displayContactDetails(addressBook);
+  });
+};
+
+$(document).ready(function() {
+  attachContactListeners();
+  $("form#new-contact").submit((event) => {
+    event.preventDefault();
+    const inputtedFirstName = $("input#new-first-name").val();
+    const inputtedLastName = $("input#new-last-name").val();
+    const inputtedPhoneNumber = $("input#new-phone-number").val();
+    const inputtedAddress = $("input#new-address").val();
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input#new-phone-number").val("");
+    $("input#new-address").val("");
+
+
+    let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedAddress);
+    addressBook.addContact(newContact);
+    displayContactDetails(addressBook);
+  });
+});
+
+
+const list = document.querySelector("#contacts");
+
+const searchBar = document.forms['search-person'].querySelector('input');
+searchBar.addEventListener('keyup', function(event){
+    const search = event.target.value.toLowerCase();
+    const person = list.getElementById('id');
+    Array.from(person).forEach(function(per){
+        const name = per.firstElementChild.textContent;
+        if(name.toLowerCase().indexOf(search) != -1){
+           per.style.display = 'block'; 
+        } else {
+            per.style.display = 'none'; 
+        }
+    })
+})
