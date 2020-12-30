01ec3855da0088488ea915cf862fea6b11e9c457
acedmiston <63328695+acedmiston@users.noreply.github.com>
2020-12-30 19:09:04 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,42 +1,42 @@
 function AddressBook() {
-    this.contacts = {};
-    this.currentId = 0;
+  this.contacts = {};
+  this.currentId = 0;
 }
 
 // add a contact
-AddressBook.prototype.addContact = function(contact) {
-    contact.id = this.assignId();
-    this.contacts[contact.id] = contact;
+AddressBook.prototype.addContact = function (contact) {
+  contact.id = this.assignId();
+  this.contacts[contact.id] = contact;
 }
 
 //assign a unique id
-AddressBook.prototype.assignId = function() {
-    this.currentId += 1;
-    return this.currentId;
+AddressBook.prototype.assignId = function () {
+  this.currentId += 1;
+  return this.currentId;
 }
 
 // find a contact
-AddressBook.prototype.findContact = function(id) {
-    if (this.contacts[id] != undefined) {
-      return this.contacts[id];
-    }
+AddressBook.prototype.findContact = function (id) {
+  if (this.contacts[id] != undefined) {
+    return this.contacts[id];
+  }
+  return "This contact does not exist";
+}
+
+// delete a contact
+AddressBook.prototype.deleteContact = function (id) {
+  if (this.contacts[id] === undefined) {
     return "This contact does not exist";
   }
-  
-// delete a contact
-  AddressBook.prototype.deleteContact = function(id) {
-    if (this.contacts[id] === undefined) {
-      return "This contact does not exist";
-    }
-    delete this.contacts[id];
-    return true;
-  }
+  delete this.contacts[id];
+  return true;
+}
 
-function Contact(firstName, lastName, phoneNumber, address){
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.phoneNumber = phoneNumber;
-    this.address = address;
+function Contact(firstName, lastName, phoneNumber, address) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.phoneNumber = phoneNumber;
+  this.address = address;
 }
 
 let addressBook = new AddressBook();
@@ -45,7 +45,7 @@ let addressBook = new AddressBook();
 function displayContactDetails(addressBookToDisplay) {
   let contactsList = $("ul#contacts");
   let htmlForContactInfo = "";
-  Object.keys(addressBookToDisplay.contacts).forEach(function(key) {
+  Object.keys(addressBookToDisplay.contacts).forEach(function (key) {
     const contact = addressBookToDisplay.findContact(key);
     htmlForContactInfo += "<li class='people' id=" + contact.id + ">" + contact.firstName + " " + contact.lastName + "</li>"
   });
@@ -62,23 +62,23 @@ function showContact(contactId) {
   $(".address").html(contact.address);
   let buttons = $("#buttons");
   buttons.empty();
-  buttons.append("<button class='deleteButton' id=" +  + contact.id + ">Delete</button>");
+  buttons.append("<button class='deleteButton' id=" + + contact.id + ">Delete</button>");
 }
 
 //show contact on right side when clicked
 function attachContactListeners() {
-  $("ul#contacts").on("click", "li", function() {
+  $("ul#contacts").on("click", "li", function () {
     showContact(this.id);
   });
 
-  $("#buttons").on("click", ".deleteButton", function() {
+  $("#buttons").on("click", ".deleteButton", function () {
     addressBook.deleteContact(this.id);
     $("#show-contact").hide();
     displayContactDetails(addressBook);
   });
 };
 
-$(document).ready(function() {
+$(document).ready(function () {
   attachContactListeners();
   $("form#new-contact").submit((event) => {
     event.preventDefault();
@@ -104,16 +104,16 @@ const list = document.getElementsByClassName("people");
 const searchBar = document.forms['search-person'].querySelector('input');
 
 //prevents page from refreshing if you accidentally hit enter when searching
-document.getElementById("search-person").addEventListener("submit", function(event){
+document.getElementById("search-person").addEventListener("submit", function (event) {
   event.preventDefault();
 });
 
 //adds an event listener when typing in search field to show if there is a contact by hiding the others
-searchBar.addEventListener('keyup', function(event){
+searchBar.addEventListener('keyup', function (event) {
   const search = event.target.value.toLowerCase();
-  for(let i = 0; i < list.length; i++){
+  for (let i = 0; i < list.length; i++) {
     const name = list[i].textContent;
-    if(name.toLowerCase().indexOf(search) != -1){
+    if (name.toLowerCase().indexOf(search) != -1) {
       list[i].style.display = 'block';
     } else {
       list[i].style.display = 'none';
